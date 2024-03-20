7ec5d4ed8b04cfdef07f5878db56ad7c8f1ff4f2
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-03-21 00:21:59 +0700

    chore: redeploy 21032024

--- client-nextjs/src/pages/_app.tsx
+++ client-nextjs/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 
+// redeploy 21032024
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
